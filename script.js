a9a2b26a76da41dfb23b4b2505f6a3306b278631
Dukeabaddon <96175643+Dukeabaddon@users.noreply.github.com>
2024-08-03 00:52:32 +0800

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+const sideMenu = document.querySelector('#sideMenu');
+const navBar = document.querySelector('nav');
+const navLinks = document.querySelector('nav ul');
+
+function openMenu(){
+    sideMenu.style.transform = 'translateX(-16rem)';
+}
+function closeMenu(){
+    sideMenu.style.transform = 'translateX(16rem)';
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        darkBlue: '#141E46',
+        whiteGray: '#EEEEEE',
+        buttonGreen: '#41B06E',
+        buttonHover: '#1FA455'
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      },
+    }
+  }
+}
